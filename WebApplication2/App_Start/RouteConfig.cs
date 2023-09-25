ccacd6e78fd1eb53e57e8ae06f67502335ce116e
dxvu2205 <dxvu2205@gmail.com>
2023-09-26 02:00:01 +0700

    admin

--- WebApplication2/App_Start/RouteConfig.cs
+++ WebApplication2/App_Start/RouteConfig.cs
@@ -12,12 +12,12 @@ namespace WebApplication2
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebApplication2.Controllers" }
+);
         }
     }
 }
